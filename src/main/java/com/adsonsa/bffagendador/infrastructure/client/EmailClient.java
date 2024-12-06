e4c8d8a265ac4bd42c7028a19082efb0810ec57a
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-12-05 21:09:47 -0300

    Adicionados client de email e emailService

---
+++ src/main/java/com/adsonsa/bffagendador/infrastructure/client/EmailClient.java
@@ -0,0 +1,11 @@
+package com.adsonsa.bffagendador.infrastructure.client;
+
+import com.adsonsa.bffagendador.business.dto.out.TarefasDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "notificaco", url = "${notificaco.url}")
+public interface EmailClient {
+
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+}
